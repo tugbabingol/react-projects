55327e78d32881ae0bf65ebb97bd2144bfc6c7cf
TugbaBingol <tugbaabiingoll@gmail.com>
2024-02-13 00:00:10 +0300

    Accordian

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Accordian from './components/accordian';
+
+function App() {
+  return (
+    <div className="App">
+      {/*Accordian Component*/}
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
