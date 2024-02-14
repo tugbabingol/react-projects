8b971694f20841b6a25525969016dd2f713b6600
TugbaBingol <tugbaabiingoll@gmail.com>
2024-02-15 00:02:34 +0300

    Random Color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/*Accordian Component*/}
-      <Accordian/>
+     {/* <Accordian/>*/}
+
+      {/*Random color Component*/}
+      <RandomColor/>
     </div>
   );
 }
