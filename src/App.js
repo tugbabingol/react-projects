bddf343efec99bb100cf4a05a31632bc383c578b
TugbaBingol <tugbaabiingoll@gmail.com>
2024-02-19 19:37:17 +0300

    Image Slider

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
     <div className="App">
       {/*Accordian Component*/}
-     {/* <Accordian/>*/}
+      {/* <Accordian/>*/}
 
       {/*Random color Component*/}
       {/*<RandomColor/>*/}
 
       {/* Star rating component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image Slider Component */}
+      <ImageSlider
+        url={'https://picsum.photos/v2/list'}
+        page={"1"}
+        limit={"10"} />
+
     </div>
   );
 }
